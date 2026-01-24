92407686a42f9a08b815e8bc2f35b17f49a7412e
ggmini <minigsm14@gmail.com>
2026-01-24 22:56:54 +0100

    Test app now fetches path from a custom txt file

--- LibTest/LibTest.cpp
+++ LibTest/LibTest.cpp
@@ -1,12 +1,26 @@
 #include "../maxcsolib/Compressor.h"
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+std::string getPathFromFile(){
+    std::ifstream t("path.txt");
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    return buffer.str(); 
+}
 
 int main() {
+    std::string path = getPathFromFile(); 
+
     maxcsolib::Arguments args;
     maxcsolib::default_args(args);
-    args.inputs.push_back("test.iso");
-    args.outputs.push_back("test.cso");
+    std::cout << "Converting " << path << ".iso to " << path << ".cso" << std::endl; 
+    args.inputs.push_back(path + ".iso");
+    args.outputs.push_back(path + ".cso");
     maxcsolib::Compressor *compressor = new maxcsolib::Compressor();
     compressor->Compress(args);
     delete compressor;
